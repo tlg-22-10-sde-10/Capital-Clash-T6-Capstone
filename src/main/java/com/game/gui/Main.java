b6320e22eba82739a9a7d618f1574a2dd69aa49e
the-mighty-fall <vvarfare@gmail.com>
2023-02-21 17:25:38 -0600

    font changes

--- src/main/java/com/game/gui/Main.java
+++ src/main/java/com/game/gui/Main.java
@@ -6,8 +6,6 @@ import javax.swing.*;
 import java.io.IOException;
 
 
-
-
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         new Frame();
